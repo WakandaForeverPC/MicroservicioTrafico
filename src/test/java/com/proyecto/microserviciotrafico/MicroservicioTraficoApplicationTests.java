188c7f1d3d54d0749af5f77ced8345670bc44040
Samuu10 <svallmun@myuax.com>
2024-12-09 09:49:40 +0100

    Initial commit

---
+++ src/test/java/com/proyecto/microserviciotrafico/MicroservicioTraficoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.microserviciotrafico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioTraficoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
